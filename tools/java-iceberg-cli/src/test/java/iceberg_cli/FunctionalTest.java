641d0297c54aaa1b9b3c2db6c0fa25bb36b96bc7
Saransh Gupta <saransh@ibm.com>
2023-07-05 18:34:51 +0000

    Added test
    Signed-off-by: Saransh Gupta <saransh@ibm.com>

--- tools/java-iceberg-cli/src/test/java/iceberg_cli/FunctionalTest.java
+++ tools/java-iceberg-cli/src/test/java/iceberg_cli/FunctionalTest.java
@@ -171,14 +171,43 @@ class FunctionalTest {
             throw new ServletException("Error: " + t.getMessage(), t);
         }
     }
-    
+
     @Test
     @Order(6)
+    @DisplayName("Test the functionality of rewrite files")
+    void rewritefiles() throws ServletException {
+        try {
+            metaConn = new IcebergConnector(catalog, namespace, tablename, creds);
+            MetastoreConnector metaConnDup = new IcebergConnector(catalog, namespace, (tablename + "Dup"), creds);
+            Schema schema = SchemaParser.fromJson("{\"type\":\"struct\",\"schema-id\":0,\"fields\":[{\"id\":1,\"name\":\"ID\",\"required\":true,\"type\":\"int\"},{\"id\":2,\"name\":\"Name\",\"required\":true,\"type\":\"string\"},{\"id\":3,\"name\":\"Price\",\"required\":true,\"type\":\"double\"},{\"id\":4,\"name\":\"Purchase_date\",\"required\":true,\"type\":\"timestamp\"}]}");
+        
+            System.out.println("Running test 8...");
+            String record = "{\"records\":[{\"ID\":1,\"Name\":\"Testing\",\"Price\": 1000,\"Purchase_date\":\"2022-11-09T12:13:54.480\"}]}";
+            String dataFiles = metaConn.writeTable(record, null);
+            boolean status = metaConnDup.createTable(schema, null, false);
+            String dataFilesDup = metaConnDup.writeTable(record, null);
+            JSONObject result = new JSONObject();
+            result.put("files_to_del", new JSONObject(dataFiles).getJSONArray("files"));
+            result.put("files_to_add", new JSONObject(dataFilesDup).getJSONArray("files"));
+            status = metaConn.rewriteFiles(result.toString());
+            Assertions.assertEquals(true, status);
+            System.out.println("Test 8 completed");
+            passed_tests += 1;
+        } catch (Throwable t) {
+            failed_tests.add("rewritefiles");
+            throw new ServletException("Error: " + t.getMessage(), t);
+        }
+    }
+    
+    @Test
+    @Order(7)
     @DisplayName("Test the functionality of drop table")
     void droptable() throws ServletException {
         try {
             System.out.println("Running test 6...");
             boolean status = metaConn.dropTable();
+            Assertions.assertEquals(true, status);
+            status = metaConnDup.dropTable();
             Assertions.assertEquals(true, status);
             System.out.println("Test 6 completed");
             passed_tests += 1;
@@ -189,7 +218,7 @@ class FunctionalTest {
     }    
     
     @Test
-    @Order(7)
+    @Order(8)
     @DisplayName("Test the functionality of drop namespace")
     void dropnamespace() throws ServletException {
         try {
