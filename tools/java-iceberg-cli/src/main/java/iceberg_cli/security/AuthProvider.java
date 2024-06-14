99e643dd09e2220fa0287547f0d25bba29d0a248
s-akhtar-baig <sabaig099@gmail.com>
2024-06-14 18:33:37 -0400

    Create unique keystore for each session

--- tools/java-iceberg-cli/src/main/java/iceberg_cli/security/AuthProvider.java
+++ tools/java-iceberg-cli/src/main/java/iceberg_cli/security/AuthProvider.java
@@ -1,6 +1,10 @@
 package iceberg_cli.security;
 
+import java.io.File;
+
 import org.apache.hadoop.hive.metastore.conf.MetastoreConf;
+import org.apache.hadoop.security.alias.CredentialProviderFactory;
+import org.apache.hadoop.security.alias.JavaKeyStoreProvider;
 
 import iceberg_cli.catalog.CustomCatalog;
 
@@ -11,6 +15,18 @@ public class AuthProvider {
             new KerberosAuthenticator(catalog).login();
         } else if (!catalog.getConf(MetastoreConf.ConfVars.METASTORE_CLIENT_PLAIN_USERNAME).isEmpty()) {
             new PlainAuthenticator(catalog).login();
+        }
+    }
+    
+    public static void clearSession(CustomCatalog catalog) {
+        String providerPath = catalog.getConf(CredentialProviderFactory.CREDENTIAL_PROVIDER_PATH);
+        if (providerPath != null) {
+            String[] path = providerPath.split(JavaKeyStoreProvider.SCHEME_NAME + "://file");
+            if (path.length == 2) {
+                File file = new File(path[1]);
+                if (file.exists() && file.isFile())
+                    file.delete();
+            }
         }
     }
 }
--- tools/java-iceberg-cli/src/main/java/iceberg_cli/security/PlainAuthenticator.java
+++ tools/java-iceberg-cli/src/main/java/iceberg_cli/security/PlainAuthenticator.java
@@ -2,11 +2,11 @@ package iceberg_cli.security;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.metastore.conf.MetastoreConf;
 import org.apache.hadoop.security.alias.CredentialProvider;
-import org.apache.hadoop.security.alias.CredentialProvider.CredentialEntry;
 import org.apache.hadoop.security.alias.CredentialProviderFactory;
 import org.apache.hadoop.security.alias.JavaKeyStoreProvider;
 
@@ -16,6 +16,7 @@ public class PlainAuthenticator {
     CustomCatalog catalog;
     String username;
     String password;
+    String keystore;
     private static final String authMode = "PLAIN";
     private static final String passwordEnvVar = "METASTORE_CLIENT_PLAIN_PASSWORD";
     private static final String passwordConfVar = "hive.metastore.client.plain.password";
@@ -24,6 +25,7 @@ public class PlainAuthenticator {
     public PlainAuthenticator(CustomCatalog catalog) throws Exception {
         this.catalog = catalog;
         getCredentials();
+        keystore = String.format("hms_auth_%s.%s", UUID.randomUUID().toString(), JavaKeyStoreProvider.SCHEME_NAME);
     }
     
     public void login() throws IOException {
@@ -31,9 +33,7 @@ public class PlainAuthenticator {
         catalog.setConf(MetastoreConf.ConfVars.METASTORE_CLIENT_AUTH_MODE, authMode);
         catalog.setConf(MetastoreConf.ConfVars.EXECUTE_SET_UGI, "false");
         
-        String fileName = String.format("hms_auth.%s", JavaKeyStoreProvider.SCHEME_NAME);
-        String credUrl = JavaKeyStoreProvider.SCHEME_NAME + "://file" + dataDir + File.separator + fileName;
-        validateCredFile(credUrl);
+        String credUrl = createCredFile();
         catalog.setConf(CredentialProviderFactory.CREDENTIAL_PROVIDER_PATH, credUrl);
     }
     
@@ -45,26 +45,6 @@ public class PlainAuthenticator {
             }
         }
     }
-    
-    private synchronized void validateCredFile(String credUrl) throws IOException {
-        Configuration credConf = new Configuration();
-        credConf.set(CredentialProviderFactory.CREDENTIAL_PROVIDER_PATH, credUrl);
-        CredentialProvider provider = CredentialProviderFactory.getProviders(credConf).get(0);
-   
-       // Check if a credential entry exists for the provided username
-       CredentialEntry entry = provider.getCredentialEntry(username);
-       if (entry == null) {
-           provider.createCredentialEntry(username, password.toCharArray());
-           provider.flush();
-       } else if (!(String.valueOf(entry.getCredential()).equals(password))) {
-           // Check if the entry token is not the same as the provided token
-           // for a username then, recreate an entry. This can happen as a
-           // user can have multiple tokens.
-           provider.deleteCredentialEntry(username);
-           provider.createCredentialEntry(username, password.toCharArray());
-           provider.flush();
-       }
-   }
     
     private void getCredentials() throws Exception {
         username = catalog.getConf(MetastoreConf.ConfVars.METASTORE_CLIENT_PLAIN_USERNAME);
@@ -78,5 +58,23 @@ public class PlainAuthenticator {
         // Validate password
         if (password == null)
             throw new Exception("Password is required for plain authentication");
+    }
+    
+    private String createCredFile() throws IOException {
+        String credUrl = JavaKeyStoreProvider.SCHEME_NAME + "://file" + dataDir + File.separator + keystore;
+        Configuration credConf = new Configuration();
+        credConf.set(CredentialProviderFactory.CREDENTIAL_PROVIDER_PATH, credUrl);
+        CredentialProvider provider = CredentialProviderFactory.getProviders(credConf).get(0);
+        
+        // Cleanup if keystore already exists
+        File file = new File(dataDir, keystore);
+        if (file.exists() && file.isFile())
+            file.delete();
+    
+        // Create a credential entry for the provided username
+        provider.createCredentialEntry(username, password.toCharArray());
+        provider.flush();
+    
+        return credUrl;
     }
 }
